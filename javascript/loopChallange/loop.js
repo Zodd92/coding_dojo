7bb35af24de9238ab7e98f9239b96bf680b05cbd
Zodd92 <66090726+Zodd92@users.noreply.github.com>
2023-05-15 00:40:08 +0200

    Add files via upload

---
+++ javascript/loopChallange/loop.js
@@ -0,0 +1,25 @@
+for(var i=0; i<=20; i++){
+    if((i%2)!=0){
+        console.log(i);
+    }
+}
+
+for(var j=100; j<0;j--){
+    if((j%3)==0){
+        console.log(j);
+    }
+}
+
+for(var k=5.5; k<=-3.5; k-=1.5){
+      console.log(k);
+}
+ var sum=0;
+for(var p=1; p<=100; p--){
+        sum+=p;
+}
+console.log(p);
+var product;
+for(var o=1; o<=12; o++){
+    product*=o;
+}
+console.log(product);
---
+++ javascript/loopChallange/loop2.js
@@ -0,0 +1,5 @@
+for(var j=100; j<0;j--){
+    if((j%3)==0){
+        console.log(j);
+    }
+}
